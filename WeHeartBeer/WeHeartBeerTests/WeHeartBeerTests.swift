7c2807948594ee6b0700b33131a6d14a6daa3cf2
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-10-21 10:09:00 -0200

    init

---
+++ WeHeartBeer/WeHeartBeerTests/WeHeartBeerTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeHeartBeerTests.swift
+//  WeHeartBeerTests
+//
+//  Created by Fernando H M Bastos on 10/21/15.
+//  Copyright © 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import XCTest
+@testable import WeHeartBeer
+
+class WeHeartBeerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
