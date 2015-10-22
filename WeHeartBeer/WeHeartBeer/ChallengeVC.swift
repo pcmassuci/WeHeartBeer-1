d6373b6dc6315cadbb743cab67f2763f90dadcbf
Matheus Santos Lopes <matheus.fcc.faj@gmail.com>
2015-10-22 11:19:50 -0200

    criação da pasta Controller e ChallengeVC

---
+++ WeHeartBeer/WeHeartBeer/ChallengeVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChallengeVC.swift
+//  WeHeartBeer
+//
+//  Created by Matheus Santos Lopes on 22/10/15.
+//  Copyright © 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
