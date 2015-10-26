30bbfe8b44c60ceff42a2ca8abdc2c59a3caab54
Matheus Santos Lopes <matheus.fcc.faj@gmail.com>
2015-10-26 09:26:05 -0200

    link facebook

--- WeHeartBeer/WeHeartBeer/ChallengeVC.swift
+++ WeHeartBeer/WeHeartBeer/ChallengeVC.swift
@@ -13,19 +13,40 @@ class ChallengeVC: UIViewController {
     @IBOutlet weak var challengeImage: UIImageView!
     @IBOutlet weak var challengeTitle: UILabel!
     @IBOutlet weak var challengeDescription: UILabel!
-    @IBOutlet weak var challengeProfile: UIButton!
-    
-    
+    @IBOutlet weak var challengeFb: UIButton!
+
+   
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func challengeFb(sender: UIButton){
+        
+        
+        let fbURLWeb: NSURL = NSURL(string: "https://www.facebook.com/Matheusfccfaj")!
+        //let fbURLID: NSURL = NSURL(string: "fb://profile/719245588122387")!
+   
+        UIApplication.sharedApplication().openURL(fbURLWeb)
+//
+//        if(UIApplication.sharedApplication().canOpenURL(fbURLID)){
+//            // FB installed
+//            UIApplication.sharedApplication().openURL(fbURLID)
+//        } else {
+//            // FB is not installed, open in safari
+//            UIApplication.sharedApplication().openURL(fbURLWeb)
+//        }
+    }
+    
+    
+
     
     
     
