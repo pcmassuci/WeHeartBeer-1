0a8c627fc8a4261029a419d67d2af630191550cb
Paulo César Morandi Massuci <pcmassuci@MBP-de-Paulo.home>
2015-11-04 13:47:24 -0200

    ponto de partida, criação da breweryVC, e vinculando ela ao storyBoard

---
+++ WeHeartBeer/WeHeartBeer/BreweryVC.swift
@@ -0,0 +1,45 @@
+//
+//  BreweryVC.swift
+//  WeHeartBeer
+//
+//  Created by Paulo César Morandi Massuci on 04/11/15.
+//  Copyright © 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import UIKit
+
+class BreweryVC: UIViewController {
+    
+    @IBOutlet weak var logoBrewery: UIImageView!
+    
+    @IBOutlet weak var nameBrewery: UILabel!
+    
+    @IBOutlet weak var placeBrewery: UILabel!
+    
+    @IBOutlet weak var linkBrewery: UILabel!
+    
+    @IBOutlet weak var listOfProducts: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
