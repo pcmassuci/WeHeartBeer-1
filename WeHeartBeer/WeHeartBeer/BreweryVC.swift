e09c951a974ac4ddb1f9e90d4cab75e70aac6970
Paulo César Morandi Massuci <pcmassuci@MBP-de-Paulo.home>
2015-11-04 23:36:10 -0200

    trocando labéus, e se ambientando com o projeto, amanha corro mais

--- WeHeartBeer/WeHeartBeer/BreweryVC.swift
+++ WeHeartBeer/WeHeartBeer/BreweryVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class BreweryVC: UIViewController {
     
@@ -20,13 +21,52 @@ class BreweryVC: UIViewController {
     
     @IBOutlet weak var listOfProducts: UITableView!
     
-    
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        var pointerReceive = "4blpTUoLIz"
+        
+        var brewery = PFObject(className:"Brewery")
+        let query = PFQuery(className:"Brewery")
+        query.getObjectInBackgroundWithId(pointerReceive) {
+            (brewery: PFObject?, error: NSError?) -> Void in
+            if error == nil && brewery != nil {
+                print(brewery)
+                
+                let name = brewery!["name"] as! String
+                let place = brewery!["local"] as! String
+                let link = brewery!["contact"] as! String
+                self.updateData(name, place: place, link: link)
+            } else {
+                print(error)
+                //lembrar de colocar alerta, e ação caso nao seja encontrado a cervejaria
+                
+            }
+        }
+        
+      
+
+    }
+    
+    
+    func updateData(name:String!, place:String!, link:String!){
+        
+    
+    nameBrewery.text = name
+    placeBrewery.text = place
+    linkBrewery.text = link
+    
+ 
+    
+    
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
