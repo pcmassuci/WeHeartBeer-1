76ab8f6c93f39f42c18e39d92784715f9366d67f
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-11-06 14:20:39 -0200

    facebook SDK

--- WeHeartBeer/WeHeartBeer/AppDelegate.swift
+++ WeHeartBeer/WeHeartBeer/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreData
 import Parse
 import Bolts
+import FBSDKCoreKit
 
 //teste
 
@@ -33,9 +34,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // [Optional] Track statistics around application opens.
         PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
 
         // Override point for customization after application launch.
-        return true
+        //return true
+    }
+    
+    func application(application: UIApplication,
+        openURL url: NSURL,
+        sourceApplication: String?,
+        annotation: AnyObject) -> Bool {
+            return FBSDKApplicationDelegate.sharedInstance().application(
+                application,
+                openURL: url,
+                sourceApplication: sourceApplication,
+                annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -54,6 +68,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        FBSDKAppEvents.activateApp()
+
     }
 
     func applicationWillTerminate(application: UIApplication) {
