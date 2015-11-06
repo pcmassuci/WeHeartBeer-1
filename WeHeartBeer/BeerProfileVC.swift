f36c6fea330c72248d65f17de7b630af6782a2f6
Júlio César Garavelli <juliocesargaravelli@MacBook-Pro-de-Julio.local>
2015-11-06 14:18:37 -0200

    BeerProfileVC iniciado!

--- WeHeartBeer/BeerProfileVC.swift
+++ WeHeartBeer/BeerProfileVC.swift
@@ -8,28 +8,58 @@
 
 import UIKit
 
-class BeerProfileVC: UIViewController {
-
+class BeerProfileVC: UIViewController, FloatRatingViewDelegate {
+    
+    @IBOutlet var ratingSegmentedControl: UISegmentedControl!
+    @IBOutlet var floatRatingView: FloatRatingView!
+    @IBOutlet var liveLabel: UILabel!
+    @IBOutlet var updatedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        /** Note: With the exception of contentMode, all of these
+        properties can be set directly in Interface builder **/
+        
+        // Required float rating view params
+        self.floatRatingView.emptyImage = UIImage(named: "hops")
+        self.floatRatingView.fullImage = UIImage(named: "hopsGreen")
+        // Optional params
+        self.floatRatingView.delegate = self
+        self.floatRatingView.contentMode = UIViewContentMode.ScaleAspectFit
+        self.floatRatingView.maxRating = 5
+        self.floatRatingView.minRating = 1
+        self.floatRatingView.rating = 2.5
+        self.floatRatingView.editable = true
+        self.floatRatingView.halfRatings = true
+        self.floatRatingView.floatRatings = false
+        
+        // Segmented control init
+        //self.ratingSegmentedControl.selectedSegmentIndex = 1
+        
+        // Labels init
+        self.liveLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
+        self.updatedLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func ratingTypeChanged(sender: UISegmentedControl) {
+        self.floatRatingView.halfRatings = sender.selectedSegmentIndex==1
+        self.floatRatingView.floatRatings = sender.selectedSegmentIndex==2
     }
-    */
-
+    
+    // MARK: FloatRatingViewDelegate
+    
+    func floatRatingView(ratingView: FloatRatingView, isUpdating rating:Float) {
+        self.liveLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
+    }
+    
+    func floatRatingView(ratingView: FloatRatingView, didUpdate rating: Float) {
+        self.updatedLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
+    }
+    
 }
