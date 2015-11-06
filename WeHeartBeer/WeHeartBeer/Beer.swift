16d3af2289907c0e1347d803b61affa7191eb480
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-11-06 15:20:00 -0200

    Beer class init

---
+++ WeHeartBeer/WeHeartBeer/Beer.swift
@@ -0,0 +1,18 @@
+//
+//  Beer.swift
+//  WeHeartBeer
+//
+//  Created by Fernando H M Bastos on 11/6/15.
+//  Copyright © 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import ParseUI
+
+class Beer {
+
+
+
+
+}
