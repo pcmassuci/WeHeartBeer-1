89d573919f480c363b5652477e87a297d597b89f
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2015-11-09 09:47:28 -0200

    New Models

---
+++ WeHeartBeer/WeHeartBeer/BreweryServices.swift
@@ -0,0 +1,9 @@
+//
+//  BreweryServices.swift
+//  WeHeartBeer
+//
+//  Created by Fernando H M Bastos on 11/9/15.
+//  Copyright © 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import Foundation
