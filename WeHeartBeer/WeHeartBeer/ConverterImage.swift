aee25b0ac17e4b8cbeb334bd1c37347687ff37d8
Paulo César Morandi Massuci <pcmassuci@MacBook-Pro-de-Paulo.local>
2015-11-17 09:23:25 -0200

    mudanças

---
+++ WeHeartBeer/WeHeartBeer/ConverterImage.swift
@@ -0,0 +1,9 @@
+//
+//  ConverterImage.swift
+//  WeHeartBeer
+//
+//  Created by Paulo César Morandi Massuci on 16/11/15.
+//  Copyright © 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import Foundation
