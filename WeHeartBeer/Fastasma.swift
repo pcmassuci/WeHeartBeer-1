87c492471f82ec605d28b9553f950fe35a8c4bda
Júlio César Garavelli <juliocesargaravelli@MacBook-Pro-de-Julio.local>
2015-11-18 19:03:03 -0200

    Classe Fantasma!

---
+++ WeHeartBeer/Fastasma.swift
@@ -0,0 +1,9 @@
+//
+//  Fastasma.swift
+//  WeHeartBeer
+//
+//  Created by Júlio César Garavelli on 18/11/15.
+//  Copyright © 2015 Fernando H M Bastos. All rights reserved.
+//
+
+import Foundation
