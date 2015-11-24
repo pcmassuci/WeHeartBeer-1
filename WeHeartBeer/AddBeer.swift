4e26acd8dad330604f02c21856d308c52506bba8
Paulo César Morandi Massuci <pcmassuci@MacBook-Pro-de-Paulo.local>
2015-11-24 12:40:30 -0200

    no message

--- WeHeartBeer/AddBeer.swift
+++ WeHeartBeer/AddBeer.swift
@@ -1,17 +1,64 @@
-//
-//  AddBeer.swift
-//  WeHeartBeer
-//
-//  Created by Fernando H M Bastos on 11/19/15.
-//  Copyright © 2015 Fernando H M Bastos. All rights reserved.
-//
+    //
+    //  AddBeer.swift
+    //  WeHeartBeer
+    //
+    //  Created by Fernando H M Bastos on 11/19/15.
+    //  Copyright © 2015 Fernando H M Bastos. All rights reserved.
+    //
 
-import UIKit
-import Parse
-import ParseUI
+    import UIKit
+    import Parse
+    import ParseUI
 
-class AddBeer: UIViewController {
-    
-    
+    class AddBeer: UIViewController {
+        
+        @IBOutlet weak var nameBeer: UITextField!
+        @IBOutlet weak var nameBrewery: UITextField!
+        @IBOutlet weak var abv: UITextField!
+        @IBOutlet weak var style: UITextField!
+        
+        
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
 
-}
+            
+        }
+        
+        
+        @IBAction func saveObject(sender: AnyObject) {
+            if self.nameBeer.text != ""{
+                if self.nameBrewery.text != ""{
+                    if self.abv.text != ""{
+                       if self.style.text != ""{
+                            print("salvar")
+                            
+                        }else{
+                            print("digitar o estilo")
+                        }
+                        
+                        
+                    }else{
+                        print("digitar ABV")
+                    }
+                    
+                }else{
+                    print("digitar o nome da cervejaria")
+                }
+                
+            }else{
+                self.alertForUser("digitar o nome da cerveja")
+            }
+            
+        }
+        
+        
+        func alertForUser(message:String){
+            let alert = UIAlertController(title: "Atenção", message:message, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+    }
