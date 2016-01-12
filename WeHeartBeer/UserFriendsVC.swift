f5887324d8765c514d25cb03ff2b21843421bb4b
Paulo César Morandi Massuci <pcmassuci@MacBook-Pro-de-Paulo.local>
2016-01-12 13:08:31 -0200

    aew

--- WeHeartBeer/UserFriendsVC.swift
+++ WeHeartBeer/UserFriendsVC.swift
@@ -16,13 +16,23 @@ import ParseFacebookUtilsV4
 
 
 class UserFriendsVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        
+        
+    }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
         
-        getFBAppFriends(nil, failureHandler: {(error)
-            in print(error)})
+//        getFBAppFriends(nil, failureHandler: {(error)
+//            in print(error)})
         
         }
 
@@ -76,10 +86,32 @@ class UserFriendsVC: UIViewController {
             
         }
 
-        
+
         
         
         
         
     }
+    
+}
+
+extension UserFriendsVC: UITableViewDataSource , UITableViewDelegate{
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
+    
+    
+    
+    
 }
