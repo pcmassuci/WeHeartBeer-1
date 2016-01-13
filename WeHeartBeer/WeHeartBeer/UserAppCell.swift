90717cca63784bb7d6e529facb820c91892325fc
Paulo César Morandi Massuci <pcmassuci@MacBook-Pro-de-Paulo.local>
2016-01-13 12:29:30 -0200

    oi amiguinhos

---
+++ WeHeartBeer/WeHeartBeer/UserAppCell.swift
@@ -0,0 +1,25 @@
+//
+//  UserAppCell.swift
+//  BeerLove
+//
+//  Created by Paulo César Morandi Massuci on 13/01/16.
+//  Copyright © 2016 Fernando H M Bastos. All rights reserved.
+//
+
+import UIKit
+
+class UserAppCell: UITableViewCell {
+
+    @IBOutlet weak var name: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
