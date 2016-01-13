f307f90ceec0bad351c2df728a82aecbf74fb63b
FernandoHMBastos <fernando.hm.bastos@gmail.com>
2016-01-13 09:52:09 -0200

    morevc

--- WeHeartBeer/MoreVC.swift
+++ WeHeartBeer/MoreVC.swift
@@ -10,5 +10,17 @@ import Foundation
 import UIKit
 
 class MoreVC: UIViewController {
+    
+    @IBOutlet weak var optionsMore: UITableView!
+    @IBOutlet weak var titleMore: UILabel!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 255.0/255.0, green: 192.0/255.0, blue: 3.0/255.0, alpha: 1.0)
+        
+    }
+    
+    
 
 }
