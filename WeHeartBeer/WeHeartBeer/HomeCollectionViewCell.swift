ccb6cd22374a0ca1126bc926a23e4b766573fc84
Paulo César Morandi Massuci <pcmassuci@MBP-de-Paulo.home>
2016-02-05 00:58:30 -0200

    remoçao colecion classe review

---
+++ WeHeartBeer/WeHeartBeer/HomeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewCell.swift
+//  BeerLove
+//
+//  Created by Paulo César Morandi Massuci on 04/02/16.
+//  Copyright © 2016 Fernando H M Bastos. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+}
