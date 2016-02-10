5c15a351def18ba6540484e97407840326a267a0
Fernando H M Bastos <fernando.hm.bastos@gmail.com>
2016-02-10 17:27:18 -0200

    storyBoard + ConfigVC + NavConfigVC

---
+++ WeHeartBeer/NavConfigVC.swift
@@ -0,0 +1,21 @@
+//
+//  NavConfigVC.swift
+//  BeerLove
+//
+//  Created by Fernando H M Bastos on 2/10/16.
+//  Copyright © 2016 Fernando H M Bastos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavConfigVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.hidden = false
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 250.0/255.0, green: 170.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+    }
+}
