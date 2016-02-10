5de2b6655d9b4158998bab236f21405368d6c029
Fernando H M Bastos <fernando.hm.bastos@gmail.com>
2016-02-10 15:43:41 -0200

    user profile bug fixing challenge vc bug fixing home collection UX storyboard

--- WeHeartBeer/WeHeartBeer/ChallengeVC.swift
+++ WeHeartBeer/WeHeartBeer/ChallengeVC.swift
@@ -27,7 +27,6 @@ class ChallengeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.challengeImage.image = UIImage(named:"now-pouring")
         //self.view.addSubview(challengeImage)
         
         challengeTitle.text = "Challenge"
