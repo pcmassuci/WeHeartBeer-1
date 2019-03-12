dedb60210ca42beaf23c36d56474b7e43ba55490
Paulo César Morandi Massuci <pcmassuci@gmail.com>
2019-03-12 00:52:53 -0300

    Arrumando a casa 2

--- WeHeartBeer/WeHeartBeer/ImageServices.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ImageServices.swift
-//  BeerLove
-//
-//  Created by Paulo César Morandi Massuci on 25/01/16.
-//  Copyright © 2016 Fernando H M Bastos. All rights reserved.
-//
-
-import Foundation
---
+++ WeHeartBeer/WeHeartBeer/UIColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtension.swift
+//  BeerLove
+//
+//  Created by Paulo César Morandi Massuci on 11/03/19.
+//  Copyright © 2019 Fernando H M Bastos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var yellowBeer: UIColor {
+       return UIColor(red: 250.0/255.0, green: 170.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    }
+}
